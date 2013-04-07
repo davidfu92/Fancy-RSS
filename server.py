8c6b9c9c59186bdac50677bea15b3cf264a72a43
root <root@fretznfureader.(none)>
2013-04-07 12:16:03 +0000

    add get and post to socket

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask.ext import restful
 from pymongo import MongoClient
 from datetime import datetime
@@ -8,7 +9,7 @@ import json
 #Standard MongoDB Format
 #article../ lastaccess: string, title: string, author: string, pubdate: string,
 #description: string, url: string, imageurl: array<string>, read: int, rsstitle: string
-#rss../ url: string, title: strig
+#rss../ url: string, title: string, test: string
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 time = datetime.now()
 time = time.strftime(DATETIME_FORMAT)
@@ -18,23 +19,20 @@ client = MongoClient()
 db = client.rssdb
 article = db.article
 rss = db.rss
-@app.route("/")
-def hello():
-	return "hello"
-@app.route("/url/<website>/title/<webtitle>")
-def subscribe(website, webtitle):
-	
-	look = rss.find_one({"url": website},{"_id": 0})
+
+@app.route("/", methods=['GET', 'POST'])
+def subscribe():
+	test = request.json
+	print test
+	look = rss.find_one({"url": test['url']},{"_id": 0})
 	if look is None:
-		sub = { "url": website, "title": webtitle}
-		rss.insert(sub)
+		rss.insert(test)
 		return "1"
 	else:
 		return "0"
 
 @app.route("/read/<name>")
-def read (name):
-	look = article.find_one({"url": name},{"_id": 0})
+def getArticle(name):
 	if look is None:
 		look = article.find_one({"url": name},{"lastaccess": 0, "title": 0, "author": 0, "pubdate": 0, "description": 0, "url": 0, "imageurl": 0, "read": 0, "rsstitle": 0})
 		db.article.update(look, {"read": 1})
@@ -47,14 +45,14 @@ def pull(link):
 	fullarticle = article.find({"url": link}, {"_id": 0})
 	for art in fullarticle:
 		array.append(art)
-	end = ''.join(array)
+	end = ''.join(json.dumps(array))
 	return end
 @app.route("/pullfeed")
 def feed():
 	array = []
-	fullfeed = rss.find({"_id": 0})
+	fullfeed = rss.find({"test": "set"},{"_id": 0})
 	for feed in fullfeed:
-		array.append(feed)
+		array.append(json.dumps(feed))
 	end = ''.join(array)
 	return end
-app.run()
+app.run(host='192.81.208.114')
